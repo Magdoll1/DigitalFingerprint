8bd5fdbed6e8bcfa8369382a97fb3892437235e9
Magdoll <magdoll@gmail.com>
2011-01-29 17:34:30 -0800

    ReadSampler.py runs correctly

--- ReadSampler.py
+++ ReadSampler.py
@@ -3,27 +3,26 @@ import sys
 import glob
 import SILVA
 from DiversityIndex import DiversityIndexRunner
-from Read import Read, ReadsDict, ReadsSampler, RefMap
+from Read import Read, ReadsDict, ReadsSampler, RefMap, ReadDF
 from utils.versatile_open import versatile_open_func_wrap
-
-refmap = None #RefMap(os.environ['PCODE'] + '/Silva/SILVA104.fece_augmented.fasta.gap_map.bz2', aln_length=50000)
 
 ecoli_map = SILVA.Ecoli1542_SILVA100
 subsample_sizes = [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120]
 di_runner = DiversityIndexRunner()
-ecoli_lo, ecoli_hi = None, None
 
 @versatile_open_func_wrap([0],['filename'])
-def main(filename, options, f):
+def main(filename, options, f, refmap, ecoli_lo, ecoli_hi):
+	from Read import ReadDF
 	print >> sys.stderr, "processing", filename
-	readdict = Read.ReadsDict(refmap)
-	sampler = ReadSampler(readdict)
-
+	readdict = ReadsDict(refmap)
+	readdict.read_bowtie_output(filename)
 	# need to remove all reads that are NOT within the E.coli range
 	to_del = filter(lambda x: x < ecoli_map[ecoli_lo] or x >= ecoli_map[ecoli_hi], readdict.M)
 	for x in to_del:
 		del readdict.M[x]
 	print >> sys.stderr, "now {0} reads left".format(readdict.count)
+
+	sampler = ReadsSampler(readdict)
 
 	for se in subsample_sizes:
 		for iter in xrange(options.iterations):
@@ -31,7 +30,7 @@ def main(filename, options, f):
 			df = sampler.subsample(se)
 			di = di_runner.run(df, method='Simpson', threshold=0)
 			f.write("{name},{size},{di}\n".format(\
-					name=pyro.name,\
+					name=filename,\
 					size=se,\
 					di=",".join(str(di[i]) for i in ecoli_map[ecoli_lo:ecoli_hi])))
 	# calc & write the real di
