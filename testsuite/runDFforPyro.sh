e1c4baecd8325ccfcadb791e8bb8df39f56ce55d
Magdoll <magdoll@gmail.com>
2011-06-21 12:58:42 -0700

    Pyro almost done

---
+++ testsuite/runDFforPyro.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REF_FILE=silva.bacteria/silva.bacteria.fasta
+DF_DIR=../
+INPUT_DIR=16Spyro_28F_200seq
+
+if [ ! -f $REF_FILE ]; then
+	echo "Downloading SILVA reference seed alignment from Mothur website...."
+	wget http://www.mothur.org/w/images/9/98/Silva.bacteria.zip
+	unzip Silva.bacteria.zip
+	REF_FILE=silva.bacteria/silva.bacteria.fasta
+fi
+
+if [ -d $INPUT_DIR ]; then
+	base=`basename $INPUT_DIR`
+	for FILE in `ls $base/*.fna`
+	do 
+		mothur "#align.seqs(candidate=$FILE, template=$REF_FILE, processors=4, flip=F)"
+	done
+	python $DF_DIR/Pyro.py -p "$base/*.align" --dup-ok -o $base/$base.DF
+	echo "DF file written to $base/$base.DF"
+	echo "Already know recommended E.coli range is 20-342. Running clustering...."
+	python ../clustering.py -f $base/$base.DF -r 20,342 -i Simpson -d $base/$base.Simpson20to342.DI.txt \
+		-o $base/$base.Simpson20to342.tree
+else
+	echo "$INPUT_DIR is not a directory! Terminate."
+fi
+
