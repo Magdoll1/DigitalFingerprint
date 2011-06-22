ef7b09e88a8096d23123d38ec0db5b61a522d5ca
Magdoll <magdoll@gmail.com>
2011-06-22 15:06:45 -0700

    Properly commented out Cython import

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
 
 #ext_modules = [Extension("hello", ["ReadDF.pyx"])]
 #
