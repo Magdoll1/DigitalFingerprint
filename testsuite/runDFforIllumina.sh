7ac2cdde010c5cfafde82a720a8ab22ef1c01b1f
Magdoll <magdoll@gmail.com>
2011-06-22 11:23:34 -0700

    Read Pyro almost done. To test in recycle.cs

--- testsuite/runDFforIllumina.sh
+++ testsuite/runDFforIllumina.sh
@@ -10,8 +10,8 @@ if [ -d $INPUT_DIR ]; then
 #	python $DF_DIR/Read_processbowtie.py -p "$base/*.bowtied" -o $base/$base.DF
 	echo "DF file written to $base/$base.DF"
 	echo "Already know recommended E.coli range is 641-1356. Running clustering...."
-	python $DF_DIR/clustering.py -f $base/$base.DF -r 641,1356 -i Entropy \
-		-d $base/$base.Entropy641to1356.DI.txt -o $base/$base.Entropy641to1456.tree
+	echo "python $DF_DIR/clustering.py -f $base/$base.DF -r 641,1356 -i Entropy \
+		-d $base/$base.Entropy641to1356.DI.txt -o $base/$base.Entropy641to1456.tree"
 else
 	echo "$INPUT_DIR is not a directory! Terminate."
 fi
